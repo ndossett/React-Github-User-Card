d12c998d8aa1681f5629ee416218be5fa71533d0
ndossett <niki.m.dossett@gmail.com>
2020-12-01 21:12:36 -0500

    rendered user card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import fetchUserData from './components/FetchUserData'
+// import UserCard from './components/UserCard'
 
 class App extends React.Component {
   state = {
@@ -9,25 +10,32 @@ class App extends React.Component {
 
   componentDidMount() {
     fetchUserData('ndossett')
-    .then((res) => {
-      this.setState({
-        userData:res.data
+      .then((res) => {
+        this.setState({
+          userData: res.data
+        })
+        console.log(res.data)
       })
-    })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   render() {
+    // console.log(this.state.userData)
     return(
       <div>
         <h1>Github User Profile</h1>
         <div>
-
+          <img src={this.state.userData.avatar_url} alt='user'/>
+          <div>
+            <h3>Name:{this.state.userData.name}</h3>
+            <h3>Url:{this.state.userData.url}</h3>
+          </div>
         </div>
       </div>
     )
   }
-
-
 
 }
 
